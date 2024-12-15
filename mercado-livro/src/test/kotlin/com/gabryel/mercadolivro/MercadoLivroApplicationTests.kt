094a240f07a9176d7c916ab512bf3626ec41d93b
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-15 20:47:39 -0300

    Criando estrutura base para o desenvolvimento em Kotlin

---
+++ mercado-livro/src/test/kotlin/com/gabryel/mercadolivro/MercadoLivroApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabryel.mercadolivro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
