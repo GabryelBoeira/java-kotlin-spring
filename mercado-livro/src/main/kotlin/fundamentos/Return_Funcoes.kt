a52867eb15964798ea4ed91335473930c520771f
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-15 21:51:01 -0300

    Laços de repetição

---
+++ mercado-livro/src/main/kotlin/fundamentos/Return_Funcoes.kt
@@ -0,0 +1,17 @@
+package fundamentos
+
+fun retornaNumeroPorExtenso(numero: Int): String? {
+    return when (numero) {
+        1 -> "Um"
+        2 -> "Dois"
+        3 -> "Tres"
+        else -> null
+    }
+}
+
+fun main() {
+    println(retornaNumeroPorExtenso(1))
+    println(retornaNumeroPorExtenso(2))
+    println(retornaNumeroPorExtenso(3))
+    println(retornaNumeroPorExtenso(6))
+}
