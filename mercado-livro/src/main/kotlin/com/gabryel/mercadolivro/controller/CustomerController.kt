9cbad6fe2a7357e72a07ef9352b6a2f1ae1181cc
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-16 19:13:23 -0300

    Melhorias para simplificar o código

--- mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/controller/CustomerController.kt
@@ -2,7 +2,9 @@ package com.gabryel.mercadolivro.controller
 
 import com.gabryel.mercadolivro.dto.CustomerDetailDTO
 import com.gabryel.mercadolivro.dto.CustomerSaveDTO
+import com.gabryel.mercadolivro.dto.CustomerUpdateDTO
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -19,35 +22,42 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController {
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     fun getAllCustomers(): List<CustomerDetailDTO> {
-        return listOf(CustomerDetailDTO("123", "teste 1", "teste1@gmail.com"),
-                      CustomerDetailDTO("456", "teste 2", "teste2@gmail.com"))
+        return listOf(
+            CustomerDetailDTO("123", "teste 1", "teste1@gmail.com"),
+            CustomerDetailDTO("456", "teste 2", "teste2@gmail.com")
+        )
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     fun getCustomer(@PathVariable id: String): CustomerDetailDTO {
-
         return CustomerDetailDTO("123", "1233", "teste@gmail.com")
     }
 
     @PostMapping
-    fun saveCustomer(@Valid @RequestBody customer: CustomerSaveDTO): ResponseEntity<CustomerDetailDTO> {
-        return ResponseEntity.ok(CustomerDetailDTO("123", "1233", "teste@gmail.com"))
+    @ResponseStatus(HttpStatus.CREATED)
+    fun saveCustomer(@Valid @RequestBody customer: CustomerSaveDTO) {
+        println("salvou")
     }
 
     @DeleteMapping("/{id}")
-    fun deleteCustomer(@PathVariable id: String): ResponseEntity<Void>{
-        return ResponseEntity.accepted().build()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCustomer(@PathVariable id: String){
+        println("Pacth")
     }
 
     @PatchMapping("/{id}")
-    fun updateStatus(@PathVariable id: String) : ResponseEntity<Void>{
-        return ResponseEntity.accepted().build()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateStatus(@PathVariable id: String) {
+        println("Pacth")
     }
 
     @PutMapping("/{id}")
-    fun updateCustomer(@PathVariable id: String) : ResponseEntity<Void>{
-        return ResponseEntity.accepted().build()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateCustomer(@PathVariable id: String, @Valid @RequestBody update: CustomerUpdateDTO) {
+        println("atualizar")
     }
-    
+
 }
