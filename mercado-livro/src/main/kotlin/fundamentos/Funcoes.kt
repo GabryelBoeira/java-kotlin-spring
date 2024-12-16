2ee4afd8a3f0ca1434cebdd5e6bc373da17c9bea
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-15 21:06:40 -0300

    Funções, Retornos e Parâmetros

---
+++ mercado-livro/src/main/kotlin/fundamentos/Funcoes.kt
@@ -0,0 +1,25 @@
+package fundamentos;
+
+//Funcao que retorna void
+fun main() {
+    println(getString())
+    println(soma(2, 3))
+    println(getDados("Thor", 20))
+    println(getDados(idade = 25, nome = "Thor"))
+    println(getDados("Thor")) //Idade padrao()
+}
+
+//Funcao que retorna uma String
+fun getString(): String {
+    return "Hello World"
+}
+
+//Funcao com retorno e recebendo paramentro a e b
+fun soma(a: Int, b: Int): Int {
+    return a + b
+}
+
+//Concaternando String
+fun getDados(nome: String, idade: Int = 0): String {
+    return "Nome: $nome, Idade: $idade"
+}
