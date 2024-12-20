bada89f8865df9cf83e5bb3ba8364c186c98a5b5
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-19 22:14:13 -0300

    Flyway nao estava executando, adicionando configurações

---
+++ mercado-livro/src/main/resources/db.migration/V2__create_table_book.sql
@@ -0,0 +1,9 @@
+#Criando a tabela de book
+CREATE TABLE IF NOT EXISTS book (
+    id BIGINT NOT NULL AUTO_INCREMENT COMMENT 'identificador do livro',
+    name VARCHAR(100) NOT NULL COMMENT 'Nome do livro',
+    price decimal(10,2) NOT NULL COMMENT 'Preço do livro',
+    customer_id BIGINT NOT NULL COMMENT 'Identificador do cliente',
+    PRIMARY KEY (id),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
