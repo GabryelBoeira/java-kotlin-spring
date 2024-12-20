2f23d0276c73792c837ad779f492fef052ecea27
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-19 23:53:45 -0300

    Bloqueando a atualização de status após a sua seleção

--- mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/model/BookModel.kt
+++ mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/model/BookModel.kt
@@ -13,12 +13,27 @@ data class BookModel(
     var name: String,
     var price: BigDecimal,
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
-
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: CustomerModel? = null
+) {
 
-)
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == BookStatus.DELETED || field == BookStatus.CANCELLED)
+                throw Exception("Book cannot be update status to $field")
+            field = value
+        }
+
+    constructor(
+        id: Long? = null,
+        name: String?, price:
+        BigDecimal?,
+        status: BookStatus?,
+        customer: CustomerModel?
+    ) : this(id = id, name = name ?: "", price = price ?: BigDecimal.ZERO, customer = customer) {
+        this.status = status ?: this.status
+    }
+}
