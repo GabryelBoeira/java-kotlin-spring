ebfac939e351594d9132336f8dff8dafbe07b3db
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-21 20:11:00 -0300

    Criando a tabela de roles dos usuários do sistema

---
+++ mercado-livro/src/main/resources/db.migration/V6__create_table_roles.sql
@@ -0,0 +1,7 @@
+create table roles (
+    customer_id bigint       not null comment 'Id do usuário',
+    role        varchar(50) not null comment 'Papel do usuário'
+);
+
+ALTER TABLE roles
+    ADD CONSTRAINT fk_roles_on_customer FOREIGN KEY (customer_id) REFERENCES book (id);
