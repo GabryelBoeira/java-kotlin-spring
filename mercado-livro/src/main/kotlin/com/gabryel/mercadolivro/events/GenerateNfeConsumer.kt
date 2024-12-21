df0c91ded0b15605888b772dad8c2689c38f6eaf
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-21 15:56:39 -0300

    mudando consumer para ser executando de forma assíncrona

--- mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/events/GenerateNfeConsumer.kt
+++ mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/events/GenerateNfeConsumer.kt
@@ -2,8 +2,11 @@ package com.gabryel.mercadolivro.events
 
 import com.gabryel.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class GenerateNfeConsumer (
     private val purchaseService: PurchaseService
 ) {
@@ -13,6 +16,7 @@ class GenerateNfeConsumer (
      *
      * @param purchaseEvent the event containing the purchase model.
      */
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEventProducer) {
         val nfe = UUID.randomUUID().toString()
--- mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/events/UpdateSoldBookConsumer.kt
+++ mercado-livro/src/main/kotlin/com/gabryel/mercadolivro/events/UpdateSoldBookConsumer.kt
@@ -3,8 +3,11 @@ package com.gabryel.mercadolivro.events
 import com.gabryel.mercadolivro.service.BookService
 import com.gabryel.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class UpdateSoldBookConsumer(
     private val bookService: BookService
 ) {
@@ -14,6 +17,7 @@ class UpdateSoldBookConsumer(
      *
      * @param purchaseEvent the event containing the purchase model.
      */
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEventProducer) {
         bookService.updateSoldBook(purchaseEvent.purchaseModel.books)
