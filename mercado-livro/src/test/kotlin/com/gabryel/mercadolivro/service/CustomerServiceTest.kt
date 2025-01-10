edeb668a217e962ba71d6505282a072d8d247b78
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-01-10 12:53:52 -0300

    realizando teste de outros métodos

--- mercado-livro/src/test/kotlin/com/gabryel/mercadolivro/service/CustomerServiceTest.kt
+++ mercado-livro/src/test/kotlin/com/gabryel/mercadolivro/service/CustomerServiceTest.kt
@@ -2,6 +2,7 @@ package com.gabryel.mercadolivro.service
 
 import com.gabryel.mercadolivro.enums.CustomerStatus
 import com.gabryel.mercadolivro.enums.Role
+import com.gabryel.mercadolivro.exception.NotFoundException
 import com.gabryel.mercadolivro.model.CustomerModel
 import com.gabryel.mercadolivro.repository.CustomerRepository
 import io.mockk.every
@@ -10,6 +11,7 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
@@ -25,31 +27,10 @@ class CustomerServiceTest {
     private lateinit var customerRepository: CustomerRepository
 
     @MockK
-    private lateinit var passwordEncoder : BCryptPasswordEncoder
+    private lateinit var passwordEncoder: BCryptPasswordEncoder
 
     @InjectMockKs
     private lateinit var customerService: CustomerService
-
-    /**
-     * Builds a list of fake customer models for testing purposes.
-     *
-     * @return a list of 5 [CustomerModel] instances with unique IDs and emails.
-     */
-    private fun buildCustomerList() : List<CustomerModel> {
-        return buildList {
-            for (i in 1..5) {
-                add(CustomerModel(
-                    id = i.toLong(),
-                    name = "customer name $i",
-                    email = "$i${UUID.randomUUID()}@email.com",
-                    status = CustomerStatus.ACTIVE,
-                    password = "password$i",
-                    roles = setOf(Role.CUSTOMER)
-                ))
-            }
-        }
-    }
-
 
     @Test
     fun `should return all customers pageable when name is null`() {
@@ -62,7 +43,7 @@ class CustomerServiceTest {
 
         assertNotNull(customersPage)
         verify(exactly = 1) { customerRepository.findAll(any()) }
-        verify(exactly = 0) { customerRepository.findAllByNameContainsIgnoreCase(any() ,any()) }
+        verify(exactly = 0) { customerRepository.findAllByNameContainsIgnoreCase(any(), any()) }
     }
 
     @Test
@@ -80,5 +61,93 @@ class CustomerServiceTest {
         verify(exactly = 1) { customerRepository.findAllByNameContainsIgnoreCase(null, nameCustomer) }
     }
 
+    @Test
+    fun `should create customer and encrypt password`() {
+        val initialPassword = Random().nextInt().toString()
+        val fakeCustomer = buildCustomer(password = initialPassword)
+        val fakePassword = UUID.randomUUID().toString()
+        val fakeCustomerEncrypted = fakeCustomer.copy(password = fakePassword)
+
+        every { customerRepository.save(fakeCustomerEncrypted) } returns fakeCustomer
+        every { passwordEncoder.encode(initialPassword) } returns fakePassword
+
+        customerService.save(fakeCustomer)
+
+        verify(exactly = 1) { customerRepository.save(fakeCustomerEncrypted) }
+        verify(exactly = 1) { passwordEncoder.encode(initialPassword) }
+    }
+
+    @Test
+    fun `should return customer by id`() {
+        val id = Random().nextInt().toLong()
+        val fakeCustomer = buildCustomer(id = id)
+
+        every { customerRepository.findById(id) } returns Optional.of(fakeCustomer)
+
+        val customer = customerService.getByIdCustomerModel(id)
+
+        assertEquals(fakeCustomer, customer)
+        verify(exactly = 1) { customerRepository.findById(id) }
+    }
+
+    @Test
+    fun `should throw error when customer not found`() {
+        val id = Random().nextInt().toLong()
+
+        every { customerRepository.findById(id) } returns Optional.empty()
+
+        val error = assertThrows<NotFoundException> {
+            customerService.getByIdCustomerDTO(id)
+        }
+
+        assertEquals("Customer not found for [${id}]", error.message)
+        assertEquals("ml-[get]-customer_not_found", error.internalCode)
+        verify(exactly = 1) { customerRepository.findById(id) }
+    }
+
+    /**
+     * Builds a list of fake customer models for testing purposes.
+     *
+     * @return a list of 5 [CustomerModel] instances with unique IDs and emails.
+     */
+    private fun buildCustomerList(): List<CustomerModel> {
+        return buildList {
+            for (i in 1..5) {
+                add(
+                    CustomerModel(
+                        id = i.toLong(),
+                        name = "customer name $i",
+                        email = "$i${UUID.randomUUID()}@email.com",
+                        status = CustomerStatus.ACTIVE,
+                        password = "password$i",
+                        roles = setOf(Role.CUSTOMER)
+                    )
+                )
+            }
+        }
+    }
+
+    /**
+     * Builds a fake customer model for testing purposes.
+     *
+     * @property id the customer's ID. If null, a new UUID will be generated.
+     * @property name the customer's name. Defaults to "customer name".
+     * @property email the customer's email. Defaults to a random UUID followed by "@email.com".
+     * @property password the customer's password. Defaults to "password".
+     * @return a new [CustomerModel] instance with the given properties.
+     */
+    private fun buildCustomer(
+        id: Long? = null,
+        name: String = "customer name",
+        email: String = "${UUID.randomUUID()}@email.com",
+        password: String = "password"
+    ) = CustomerModel(
+        id = id,
+        name = name,
+        email = email,
+        status = CustomerStatus.ACTIVE,
+        password = password,
+        roles = setOf(Role.CUSTOMER)
+    )
 
 }
