0a6a4d154af1bc24351019dca49fde694695bbdf
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-01-09 21:28:08 -0300

    configurando jacoco para cobertura de código

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	kotlin("plugin.jpa") version "1.9.25"
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
+	jacoco
 }
 
 group = "com.gabryel"
