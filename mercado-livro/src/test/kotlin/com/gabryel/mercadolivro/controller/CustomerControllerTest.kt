907fb3dea16fbd89a3a4ade5541571ec6fa9c3d4
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-01-26 18:08:09 -0300

    Realizando mais testes da CustomerController

--- mercado-livro/src/test/kotlin/com/gabryel/mercadolivro/controller/CustomerControllerTest.kt
+++ mercado-livro/src/test/kotlin/com/gabryel/mercadolivro/controller/CustomerControllerTest.kt
@@ -61,6 +61,59 @@ class CustomerControllerTest {
             .andExpect(jsonPath("$.items[1].status").value(customer2.status.name))
     }
 
+    @Test
+    fun `should filter all customers by name when get all`() {
+        val customer1 = customerRepository.save(buildCustomer(name = "Gustavo"))
+        customerRepository.save(buildCustomer(name = "Daniel"))
+
+        mockMvc.perform(get("/customers?name=Gus"))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.items.length()").value(1))
+            .andExpect(jsonPath("$.items[0].id").value(customer1.id))
+            .andExpect(jsonPath("$.items[0].name").value(customer1.name))
+            .andExpect(jsonPath("$.items[0].email").value(customer1.email))
+            .andExpect(jsonPath("$.items[0].status").value(customer1.status.name))
+    }
+
+
+    @Test
+    fun `should create customer`() {
+        val request = CustomerSaveDTO("fake name", "${Random.nextInt()}@fakeemail.com", "123456")
+
+        mockMvc.perform(post("/customers")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isCreated)
+
+        val customers = customerRepository.findAll().toList()
+        assertEquals(1, customers.size)
+        assertEquals(request.name, customers[0].name)
+        assertEquals(request.email, customers[0].email)
+    }
+
+    @Test
+    fun `should get user by id when user has the same id`() {
+        val customer = customerRepository.save(buildCustomer())
+
+        mockMvc.perform(get("/customers/${customer.id}").with(user(CustomUserDetails(customer))))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id").value(customer.id))
+            .andExpect(jsonPath("$.name").value(customer.name))
+            .andExpect(jsonPath("$.email").value(customer.email))
+            .andExpect(jsonPath("$.status").value(customer.status.name))
+    }
+
+    @Test
+    fun `should return forbidden when user has different id`() {
+        val customer = customerRepository.save(buildCustomer())
+
+        mockMvc.perform(get("/customers/0")
+            .with(user(CustomUserDetails(customer))))
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.httpCode").value(402))
+            .andExpect(jsonPath("$.message").value("Access Denied"))
+            .andExpect(jsonPath("$.internalCode").value("cannot.access.resource"))
+    }
 
 
 }
